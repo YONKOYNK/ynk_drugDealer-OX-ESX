6f18f5a2e7e11785e243e8464418f7f82c5281b8
YONKOYNK <141737146+YONKOYNK@users.noreply.github.com>
2025-07-01 23:11:52 +0200

    Update server.lua

--- ynk_drugDealer/server.lua
+++ ynk_drugDealer/server.lua
@@ -9,12 +9,32 @@ AddEventHandler('payDrugs', function (price, item, amount)
         return
     end
 
-    xPlayer.removeMoney(price)
-    xPlayer.addInventoryItem(item, amount)
+    xPlayer.removeMoney(price * amount)
+    exports.ox_inventory:AddItem(source, item, amount)
+
 
     print("la somme de : "..price.." a ete retracter ")
     print("item suivant a ete ajouter : "..item..' en quantite : '..amount)
 end)
 
 
+RegisterNetEvent('ox_inventory:useItem')
+AddEventHandler('ox_inventory:useItem', function(itemName)
+    local src = source
+    print("[DEBUG] Item utilisé:", itemName)
 
+    if itemName == 'turbozen' then
+        exports.ox_inventory:RemoveItem(src, 'turbozen', 1)
+
+    elseif itemName == 'ragerush' then
+        exports.ox_inventory:RemoveItem(src, 'ragerush', 1)
+
+    elseif itemName == 'dreamweed' then
+        exports.ox_inventory:RemoveItem(src, 'dreamweed', 1)
+
+    elseif itemName == 'nocturna' then
+        exports.ox_inventory:RemoveItem(src, 'nocturna', 1)
+    else
+        print("[DEBUG] Item non géré:", itemName)
+    end
+end)
