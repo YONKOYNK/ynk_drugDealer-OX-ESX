f487fb43c11ada3ce864e5a86caad3c9a1d039db
YONKOYNK <141737146+YONKOYNK@users.noreply.github.com>
2025-07-01 23:11:23 +0200

    Update config.lua

--- ynk_drugDealer/config.lua
+++ ynk_drugDealer/config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
 Config.drugs = {
-    {label = 'Turbozen Pill', price = 600, time = 6000},
-    {label = 'Rage Rush Pill', price = 500, time = 5000},
-    {label = 'Nocturna Pill', price = 400, time = 4000},
-    {label = 'Dream Weed Pill', price = 300, time = 3000}
+    {label = 'Turbozen Pill', price = 600, time = 6000},  --- temps 6seconde en ms
+    {label = 'Rage Rush Pill', price = 500, time = 5000},   --- temps 5seconde en ms
+    {label = 'Nocturna Pill', price = 400, time = 4000}, --- temps 4seconde en ms
+    {label = 'Dream Weed Pill', price = 300, time = 3000}   --- temps 3seconde en ms
 }
 
 Config.ped = {
